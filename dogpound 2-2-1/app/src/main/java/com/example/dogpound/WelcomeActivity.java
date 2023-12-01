d17811ace6c801a3dd991038eba16c613e40324d
makaylatajalle <152333855+makaylatajalle@users.noreply.github.com>
2023-12-01 06:48:52 -0800

    Update WelcomeActivity.java

--- dogpound 2-2-1/app/src/main/java/com/example/dogpound/WelcomeActivity.java
+++ dogpound 2-2-1/app/src/main/java/com/example/dogpound/WelcomeActivity.java
@@ -5,22 +5,26 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
-public class WelcomeActivity extends Activity {
-
-    private static final long DELAY_TIME = 2000;
+public class WelcomeActivity extends Activity 
+{
+    private static final long DELAY_TIME = 2000; //displays welcome page for 2 seconds
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) 
+    {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_welcome);
+        setContentView(R.layout.activity_welcome); //sets welcome page layout
 
         new Handler().postDelayed(new Runnable()
         {
             @Override
             public void run()
+            //goes to StartActivity when 2 seconds are finished
             {
+                //declare intent to switch to StartActivity
                 Intent startIntent = new Intent(WelcomeActivity.this, StartActivity.class);
-                startActivity(startIntent);
+                
+                startActivity(startIntent); //start StartActivity
                 finish();
             }
         }, DELAY_TIME);
